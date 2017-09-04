b05d4782794ed2c293f60175f9bd2f749396200e
Steve Miller <millerized@me.com>
2017-09-04 17:26:13 -0400

    Add base router path for <App/>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import './index.css';
 import {App} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={App}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
 registerServiceWorker();
